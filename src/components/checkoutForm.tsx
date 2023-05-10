1748cf367505abaf02abd17c7381ed0c04834bb6
rohit <raj0eshsing0909@gmail.com>
2023-05-10 23:54:33 +0530

    initial commit

--- src/components/checkoutForm.tsx
+++ src/components/checkoutForm.tsx
@@ -68,11 +68,8 @@ export default function CheckoutForm() {
       },
     });
 
-    // This point will only be reached if there is an immediate error when
-    // confirming the payment. Otherwise, your customer will be redirected to
-    // your `return_url`. For some payment methods like iDEAL, your customer will
-    // be redirected to an intermediate site first to authorize the payment, then
-    // redirected to the `return_url`.
+    
+    
     if (error.type === "card_error" || error.type === "validation_error") {
       setMessage(error.message);
     } else {
@@ -98,11 +95,8 @@ export default function CheckoutForm() {
         
       />
       <PaymentElement id="payment-element" options={paymentElementOptions} />
-      {/* <button disabled={isLoading || !stripe || !elements} id="submit">
-        <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
-        </span>
-      </button> */}
+     
+        {/* //buttons */}
       <div className="flex justify-end">
       <Button disabled={isLoading || !stripe || !elements} id="submit" className="mt-7">
       <span id="button-text">
